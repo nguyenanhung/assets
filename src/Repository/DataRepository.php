8520c9cd6eb5688995dfaa1f050a1506fe343ed3
HungNA - Technical Manager <hungan@lisod.vn>
2024-04-05 10:24:23 +0700

    Update scripts

--- src/Repository/DataRepository.php
+++ src/Repository/DataRepository.php
@@ -19,58 +19,58 @@ use nguyenanhung\Assets\Interfaces\ProjectInterface;
  */
 class DataRepository implements ProjectInterface
 {
-    const CONFIG_PATH = 'config';
-    const CONFIG_EXT  = '.php';
+	const CONFIG_PATH = 'config';
+	const CONFIG_EXT = '.php';
 
-    /**
-     * Function getVersion
-     *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 9/28/18 14:47
-     *
-     * @return string
-     */
-    public function getVersion()
-    {
-        return self::VERSION;
-    }
+	/**
+	 * Function getVersion
+	 *
+	 * @return string
+	 * @author: 713uk13m <dev@nguyenanhung.com>
+	 * @time  : 9/28/18 14:47
+	 *
+	 */
+	public function getVersion()
+	{
+		return self::VERSION;
+	}
 
-    /**
-     * Hàm lấy nội dung config được quy định trong thư mục config
-     *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 9/28/18 14:47
-     *
-     * @param string $configName Tên file config
-     *
-     * @return array|mixed
-     */
-    public static function getData($configName)
-    {
-        $path = __DIR__ . DIRECTORY_SEPARATOR . self::CONFIG_PATH . DIRECTORY_SEPARATOR . $configName . self::CONFIG_EXT;
-        if (is_file($path) && file_exists($path)) {
-            return require $path;
-        }
+	/**
+	 * Hàm lấy nội dung config được quy định trong thư mục config
+	 *
+	 * @param string $configName Tên file config
+	 *
+	 * @return array|mixed
+	 * @author: 713uk13m <dev@nguyenanhung.com>
+	 * @time  : 9/28/18 14:47
+	 *
+	 */
+	public static function getData($configName)
+	{
+		$path = __DIR__ . DIRECTORY_SEPARATOR . self::CONFIG_PATH . DIRECTORY_SEPARATOR . $configName . self::CONFIG_EXT;
+		if (is_file($path) && file_exists($path)) {
+			return require $path;
+		}
 
-        return [];
-    }
+		return [];
+	}
 
-    /**
-     * Hàm lấy nội dung Data từ 1 file bất kỳ trong hệ thống
-     *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 10/17/18 09:25
-     *
-     * @param string $filename Đường dẫn file config
-     *
-     * @return array|mixed
-     */
-    public static function getDataContent($filename)
-    {
-        if (is_file($filename) && file_exists($filename)) {
-            return require $filename;
-        }
+	/**
+	 * Hàm lấy nội dung Data từ 1 file bất kỳ trong hệ thống
+	 *
+	 * @param string $filename Đường dẫn file config
+	 *
+	 * @return array|mixed
+	 * @author: 713uk13m <dev@nguyenanhung.com>
+	 * @time  : 10/17/18 09:25
+	 *
+	 */
+	public static function getDataContent($filename)
+	{
+		if (is_file($filename) && file_exists($filename)) {
+			return require $filename;
+		}
 
-        return [];
-    }
+		return [];
+	}
 }
